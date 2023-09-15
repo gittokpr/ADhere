47670648410c1b005ff8f2a535e11fd5888be8f0
gitto <gitto@keyvalue.systems>
2023-09-15 09:48:20 +0530

    initial commit

---
+++ src/main/java/com/hashcoders/adhere/AdhereApplication.java
@@ -0,0 +1,13 @@
+package com.hashcoders.adhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hashcoders/adhere/AdhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashcoders.adhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
