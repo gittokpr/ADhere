4fa12bde942426f69dc08951090603afe5025eec
VaishnavNT <vaishnav.nt.10@gmail.com>
2023-09-15 18:00:46 +0530

    added get all listings by a host

--- src/main/java/com/hashcoders/adhere/listing/controller/ListingController.java
+++ src/main/java/com/hashcoders/adhere/listing/controller/ListingController.java
@@ -3,17 +3,12 @@ package com.hashcoders.adhere.listing.controller;
 import com.hashcoders.adhere.booking.entity.Booking;
 import com.hashcoders.adhere.listing.dto.ListingRequest;
 import com.hashcoders.adhere.listing.dto.ListingResponse;
+import com.hashcoders.adhere.listing.entity.Listing;
 import com.hashcoders.adhere.listing.service.ListingService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/listing")
@@ -34,5 +29,10 @@ public class ListingController {
     @GetMapping("/{id}/bookings")
     public List<Booking> getAllBookingForId(@PathVariable Long id) {
         return listingService.getAllBookingsForId(id);
+    }
+
+    @GetMapping()
+    public List<Listing> getListingsByAHost(@RequestParam String hostId) {
+        return listingService.getListingsByAHost(hostId);
     }
 }
--- src/main/java/com/hashcoders/adhere/listing/service/ListingService.java
+++ src/main/java/com/hashcoders/adhere/listing/service/ListingService.java
@@ -8,11 +8,12 @@ import com.hashcoders.adhere.listing.dto.ListingRequest;
 import com.hashcoders.adhere.listing.dto.ListingResponse;
 import com.hashcoders.adhere.listing.entity.Listing;
 import com.hashcoders.adhere.listing.repository.ListingRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -65,5 +66,9 @@ public class ListingService {
 
     public Listing getReferenceById(final Long listingId) {
         return listingRepository.getReferenceById(listingId);
+    }
+
+    public List<Listing> getListingsByAHost(String hostId)  {
+        return listingRepository.findByHostId(Long.parseLong(hostId));
     }
 }
